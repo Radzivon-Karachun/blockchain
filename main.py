5703e84240d71b9bf05c0dd6a237b4321d538574
Radzivon-Karachun <70255645+Radzivon-Karachun@users.noreply.github.com>
2022-10-18 22:33:10 +0200

    blockchain

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Block:
         self.timestamp = timestamp or time()
         self.data = [] if data is None else data
         self.hash = self.get_hash()
+        self.nonce = 0
         self.prevHash = None
 
     def get_hash(self):
@@ -18,9 +19,41 @@ class Block:
         hash.update(str(self.data).encode('utf-8'))
         return hash.hexdigest()
 
+    def mine(self, difficulty):
+        while self.hash[:difficulty] != '0' * difficulty:
+            self.nonce += 1
+            self.hash = self.get_hash()
+
 
 class Blockchain:
 
     def __init__(self):
         self.chain = [Block(str(int(time())))]
+        self.difficulty = 1
+        self.blockTime = 30000
 
+    def get_last_block(self):
+        return self.chain[len(self.chain) - 1]
+
+    def add_block(self, block):
+        block.prevHash = self.get_last_block().hash
+        block.hash = block.get_hash()
+        block.mine(self.difficulty)
+        self.chain.append(block)
+
+        self.difficulty += (-1, 1)[int(time()) - int(self.get_last_block().timestamp) < self.blockTime]
+
+    def is_valid(self):
+
+        for i in range(1, len(self.chain)):
+            currentBlock = self.chain[i]
+            prevBlock = self.chain[i - 1]
+
+            if (currentBlock.hash != currentBlock.get_hash() or prevBlock.hash != currentBlock.prevHash):
+                return False
+
+        return True
+
+    def __repr__(self):
+        return json.dumps([{'data': item.data, 'timestamp': item.timestamp, 'nonce': item.nonce, 'hash': item.hash,
+                            'prevHash': item.prevHash} for item in self.chain], indent=4)
